31e2e5a537c7f522117a5fddfb272ceb2b31799d
shivansh-aggarwal <shivansh.aggarwal@unthinkable.co>
2021-12-31 18:43:27 +0530

    added multiple table

--- src/App.js
+++ src/App.js
@@ -14,6 +14,29 @@ for (let i = 0; i < 50; i++) {
   students.push(student);
 }
 
+  // async function addStudentDataHandler(students) {
+  //   await fetch('https://react-http-4d12b-default-rtdb.firebaseio.com/', {
+  //       method: 'POST',
+  //       body: JSON.stringify(students),
+  //       headers: {
+  //           'Content-Type': 'application/json'
+  //       }
+  //   })
+  // }
+
+  // addStudentDataHandler(students);
+
+const studentsNew = [];
+
+for (let i = 0; i < 50; i++) {
+  let student = {
+    rollNo: i + 1,
+    name: (Math.random() + 1).toString(36).substring(7),
+    emailId: (Math.random() + 1).toString(36).substring(7)
+  };
+  studentsNew.push(student);
+}
+  
 function App() {
   let rowList;
   const onSaveRowHandler = (rowData) => {
@@ -23,10 +46,19 @@ function App() {
   return (
     <PageState>
       <div className='container'>
-        <div className='tableClass'>
-          <Table data={students} onSaveRow={onSaveRowHandler} />
+        <div className='innerClass'>
+          <div className='tableClass'>
+            <Table data={students} onSaveRow={onSaveRowHandler} />
+          </div>
+          <Tabs maxLength={students.length} />
         </div>
-        <Tabs data={rowList} maxLength={students.length} />
+
+        <div className='innerClass'>
+          <div className='tableClass'>
+            <Table data={studentsNew} onSaveRow={onSaveRowHandler} />
+          </div>
+            <Tabs maxLength={students.length} />
+        </div>
       </div>
     </PageState>
   )
